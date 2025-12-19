e30730a2aa09b0ab05856657c0903b5c57cd89b6
Your Name <148898409+bedxnn@users.noreply.github.com>
2025-12-19 01:12:24 -0500

    login and signuo

--- MyBankFrontend/src/App.jsx
+++ MyBankFrontend/src/App.jsx
@@ -1,15 +1,18 @@
 import Navbar from "./pages/Navbar"
 import "./css/navbar.css";
+import "./css/auth.css";
 import { Route,Routes } from "react-router-dom";
 import Dashboard from './pages/Dashboard';
 import Account from './pages/Account';
 import Wallet from './pages/Wallet';
+import Auth from './pages/auth';
 
 function App() {
   return (
     <>
 <Navbar/>
 <Routes>
+  <Route path="/auth" element={<Auth />} />
   <Route path = "/" element = {<Dashboard/>}/>
     <Route path = "/accounts" element = {<Account/>}/>
      <Route path = "/wallet" element = {<Wallet/>}/>
